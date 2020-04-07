14c8d538f01bbd2658a320d625eeb0f6b842642a
cristianciuciu <cri_ciu2@yahoo.com>
2020-04-07 20:50:14 +0200

    initial commit

---
+++ CurrencyExchangeServiceWCF/CurrencyExchangeServiceWCFTests/Service1Tests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CurrencyExchangeServiceWCF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyExchangeServiceWCF.Tests
+{
+    [TestClass()]
+    public class Service1Tests
+    {
+        [TestMethod()]
+        public void TestConvertCurrencyTest()
+        {
+            //Assert();
+        }
+
+        [TestMethod()]
+        public void TestGetCurrencyCodes()
+        {
+            //string[] x = GetCurrencyCodes();
+           // Assert.ThrowsException(GetCurrencyCodes());
+            Assert.AreEqual("ALL","ALL");
+        }
+        [TestMethod()]
+        public void TestGetActualConversionRate()
+        {
+            //string[] x = GetCurrencyCodes();
+            // Assert.ThrowsException(GetCurrencyCodes());
+            Assert.AreEqual("", "ALL");
+        }
+    }
+}
---
+++ CurrencyExchangeServiceWCF/WebServiceClient/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebServiceClient
+{
+    public partial class Form1 : Form
+    {
+        ServiceReference1.Service1Client SoapClient = new ServiceReference1.Service1Client();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+        public void FillComboBox(string[] array, ComboBox box)
+        {
+            foreach (string x in array)
+            {
+                box.Items.Add(x);
+            }
+        }
+        private void btn_GO_Click(object sender, EventArgs e)
+        {
+            lbl_Result.Text = SoapClient.ConvertCurrency(com_Currency.Text, tbx_Value.Text);
+
+        }
+
+        private void btn_Initialize_Click(object sender, EventArgs e)
+        {
+            FillComboBox(SoapClient.GetCurrencyCodes(), com_Currency);
+            
+        }
+    }
+}
